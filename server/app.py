55b4d6bb6ff9d4f633cbafaa31841018a3e94b9a
Glory Gweth <glorygweth6@gmail.com>
2023-10-11 17:38:40 +0300

    done

--- server/app.py
+++ server/app.py
@@ -10,25 +10,30 @@ app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
-
 migrate = Migrate(app, db)
-
 db.init_app(app)
-
 @app.route('/clear')
 def clear_session():
     session['page_views'] = 0
     return {'message': '200: Successfully cleared session data.'}, 200
-
 @app.route('/articles')
 def index_articles():
-
     pass
 
 @app.route('/articles/<int:id>')
 def show_article(id):
 
     pass
+    session['page_views'] = session.get('page_views', 0) + 1
+    if session['page_views'] > 3:
+        return {'message': 'Maximum pageview limit reached'}, 401    
+
+    article = Article.query.get(id)
+    if article:
+        return jsonify({'author': article.author, 'title': article.title, 'content': article.content, 'id': article.id, 'preview': article.preview, 'minutes_to_read': article.minutes_to_read, 'date': article.date})
+    else:
+        return 404
 
 if __name__ == '__main__':
     app.run(port=5555)
+
